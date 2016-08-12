000f632164743e6eefd246e0db6c25df942657d8
Duncan Thurston <duncan@capitald.co.nz>
2016-08-12 15:36:57 +1200

    WIP: Start of stockitem form

--- tradestock/forms.py
+++ tradestock/forms.py
@@ -1,7 +1,13 @@
 from flask_wtf import Form
-from wtforms import StringField, RadioField
+from wtforms import StringField, RadioField, DecimalField
 from wtforms.validators import DataRequired
 
 class NewJobForm(Form):
     name = StringField('name', validators=[DataRequired()])
     active = RadioField('active', choices=[('True', 'Active'), ('False', 'Inactive')], default='True', validators=[DataRequired()])
+
+class NewStockForm(Form):
+    sku = StringField('sku')
+    name = StringField('name', validators=[DataRequired()])
+    unitprice = DecimalField('unitprice', places=2)
+    quantity = DecimalField('quantity')
